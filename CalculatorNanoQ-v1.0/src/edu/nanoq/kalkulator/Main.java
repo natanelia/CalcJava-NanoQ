b31f1b4a9a2ee87fa0237414f2d3028964241327
Natan <natanelia7@gmail.com>
2015-04-08 21:07:53 +0700

    Updated Calculator.java to Main.java

---
+++ CalculatorNanoQ-v1.0/src/edu/nanoq/kalkulator/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nanoq.kalkulator;
+
+import edu.nanoq.ekspresi.ExpressionProcessor;
+
+import java.util.*;
+
+/**
+ * @author FiqieUlya
+ */
+
+public class Main {
+    public static void main(String args[]) {
+        Vector<String> sp;
+        int n = 1;
+        Scanner st = new Scanner(System.in);
+        while (n != 999) {
+            String s = st.nextLine();
+            ExpressionProcessor Perhitungan = new ExpressionProcessor(s);
+            System.out.println(Perhitungan.toString());
+        }
+    }
+}
